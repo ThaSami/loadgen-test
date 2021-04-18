7ae617e2de0fa37e786e35fb7c85696b16152e7e
sshabaneh <sshabaneh@atypon.com>
2021-04-18 05:19:29 +0300

    fix

--- entry.sh
+++ entry.sh
@@ -5,4 +5,4 @@ BRANCH_NAME=${BRANCH_NAME:-master}
 git pull --all
 git checkout ${BRANCH_NAME}
 
-gunicorn --chdir -c config.py app:app -w 7 -b 0.0.0.0:80
+gunicorn -c config.py app:app -w 7 -b 0.0.0.0:80
